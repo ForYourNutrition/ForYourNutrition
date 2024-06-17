5d9356ef6c07a6749c23e26269e2418144d912e8
Eunkyy <melon0802@naver.com>
2024-06-18 03:38:30 +0900

    :sparkles: feat: item 디테일 추가 및 ignorecase 추가

--- src/main/java/com/luckyGirls/ForYourNutrition/repository/ItemJpaRepository.java
+++ src/main/java/com/luckyGirls/ForYourNutrition/repository/ItemJpaRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.luckyGirls.ForYourNutrition.domain.Item;
@@ -14,5 +16,7 @@ import com.luckyGirls.ForYourNutrition.domain.Item;
 public interface ItemJpaRepository extends JpaRepository<Item, Integer> {
 	Item findById(int id);
 	List<Item> findAllByCategory(int category);
-	Page<Item> findByNameContaining(String name, Pageable pageable);
+	@Query("SELECT i FROM Item i WHERE LOWER(i.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+	Page<Item> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
+
 }
